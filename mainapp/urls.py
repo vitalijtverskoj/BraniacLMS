4386cd62db8026ec9d130a17c360a54b172d4472
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-09-29 00:09:44 +0300

    ending lesson-2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
 from .views import *
 
 urlpatterns = [
-    path('hello/', hello_view)
+    path('', MainPageVies.as_view()),
+    path('login/', LoginPageVies.as_view()),
+    path('doc_site/', DocSitePageVies.as_view()),
+    path('contacts/', ContactsPageVies.as_view()),
+    path('news/', NewsPageVies.as_view()),
+    path('courses_list/', CoursesListPageVies.as_view())
 ]
