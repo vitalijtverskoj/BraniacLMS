f53cc2987678801d5d5d8e9725b790c112037f58
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-09-29 23:21:14 +0300

    editing templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,24 +2,24 @@ from django.views.generic import TemplateView
 
 
 class MainPageVies(TemplateView):
-    template_name = 'mainapp/index.html'
+    template_name = "mainapp/index.html"
 
 
 class NewsPageVies(TemplateView):
-    template_name = 'mainapp/news.html'
+    template_name = "mainapp/news.html"
 
 
 class LoginPageVies(TemplateView):
-    template_name = 'mainapp/login.html'
+    template_name = "mainapp/login.html"
 
 
 class ContactsPageVies(TemplateView):
-    template_name = 'mainapp/contacts.html'
+    template_name = "mainapp/contacts.html"
 
 
 class DocSitePageVies(TemplateView):
-    template_name = 'mainapp/doc_site.html'
+    template_name = "mainapp/doc_site.html"
 
 
 class CoursesListPageVies(TemplateView):
-    template_name = 'mainapp/courses_list.html'
+    template_name = "mainapp/courses_list.html"
