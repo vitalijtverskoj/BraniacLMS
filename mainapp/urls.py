49d64f5f8ca72b063ab202e980184f9c4a830ca4
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-10-05 22:52:28 +0300

    adding email_to_link, paginator

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,10 +7,11 @@ from .views import *
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path("", MainPageVies.as_view(), name='main'),
-    path("login/", LoginPageVies.as_view(), name='login'),
-    path("doc_site/", DocSitePageVies.as_view(), name='docs'),
-    path("contacts/", ContactsPageVies.as_view(), name='contacts'),
-    path("news/", NewsPageVies.as_view(), name='news'),
-    path("courses_list/", CoursesListPageVies.as_view(), name='courses'),
+    path("", MainPageView.as_view(), name='main'),
+    path("login/", LoginPageView.as_view(), name='login'),
+    path("doc_site/", DocSitePageView.as_view(), name='docs'),
+    path("contacts/", ContactsPageView.as_view(), name='contacts'),
+    path("news/", NewsPageView.as_view(), name='news'),
+    path("news/<int:page>/", NewsWithPaginatorView.as_view(), name='news_paginator'),
+    path("courses_list/", CoursesListPageView.as_view(), name='courses'),
 ]
