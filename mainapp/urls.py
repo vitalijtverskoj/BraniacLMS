f8fe39abe2b4ba5f94e21cd09cda03225807b37e
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-10-07 22:43:38 +0300

    first commit

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
+from mainapp.apps import MainappConfig
+
 from .views import *
 
+app_name = MainappConfig.name
+
 urlpatterns = [
-    path("", MainPageVies.as_view()),
-    path("login/", LoginPageVies.as_view()),
-    path("doc_site/", DocSitePageVies.as_view()),
-    path("contacts/", ContactsPageVies.as_view()),
-    path("news/", NewsPageVies.as_view()),
-    path("courses_list/", CoursesListPageVies.as_view()),
+    path("", MainPageView.as_view(), name='main'),
+    path("login/", LoginPageView.as_view(), name='login'),
+    path("doc_site/", DocSitePageView.as_view(), name='docs'),
+    path("contacts/", ContactsPageView.as_view(), name='contacts'),
+    path("news/", NewsPageView.as_view(), name='news'),
+    path("news/<int:page>/", NewsWithPaginatorView.as_view(), name='news_paginator'),
+    path("courses_list/", CoursesListPageView.as_view(), name='courses'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,25 +1,43 @@
 from django.views.generic import TemplateView
+from datetime import datetime
 
-
-class MainPageVies(TemplateView):
+class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
 
-class NewsPageVies(TemplateView):
+class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-class LoginPageVies(TemplateView):
+        context['news_title'] = 'Новость'
+        context['description'] = 'Предварительное описание новости'
+        context['news_data'] = datetime.now()
+        context['range'] = range(5)
+
+        return context
+
+
+class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
 
 
-class ContactsPageVies(TemplateView):
+class ContactsPageView(TemplateView):
     template_name = "mainapp/contacts.html"
 
 
-class DocSitePageVies(TemplateView):
+class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
 
 
-class CoursesListPageVies(TemplateView):
+class CoursesListPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
+
+
+class NewsWithPaginatorView(NewsPageView):
+
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context['page_num'] = page
+        return context
