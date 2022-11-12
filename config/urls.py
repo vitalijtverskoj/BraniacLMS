4b1bef394ee5cf852ec8dd4ac2b349a55bbd4625
vitalijtverskoj <vitalijtverskoj@gmail.com>
2022-11-12 11:16:57 +0300

    adding locale

--- config/urls.py
+++ config/urls.py
@@ -26,6 +26,7 @@ from authapp.apps import AuthappConfig
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("i18n/", include('django.conf.urls.i18n')),
     path("", RedirectView.as_view(url="mainapp/")),
     path("social_auth/", include('social_django.urls', namespace='social')),
     path("mainapp/", include("mainapp.urls", namespace=MainappConfig.name)),
